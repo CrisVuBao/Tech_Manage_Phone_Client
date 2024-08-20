f5667467fbf8139e708f0ad80fdc95a1f56770de
Bao <vubaotech2002.gmail.com>
2024-08-20 17:11:01 +0700

    thêm input thêm sửa

--- src/app/features/repair-feature/add-repair/add-repair.component.ts
+++ src/app/features/repair-feature/add-repair/add-repair.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-repair',
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './add-repair.component.html',
   styleUrl: './add-repair.component.css'
 })
-export class AddRepairComponent {
+export class AddRepairComponent{
+
+  closeCollapse(): void {
+    const collapseElement = document.getElementById('addRowModal');
+    if (collapseElement) {
+      collapseElement.classList.remove('show');
+    }
+
+    // Đóng modal nếu modal là nguyên nhân tạo ra màn hình xám mờ
+    const modalElement = document.querySelector('.modal') as HTMLElement;
+    if (modalElement) {
+      modalElement.classList.remove('show');
+      modalElement.style.display = 'none';
+      const modalBackdrop = document.querySelector('.modal-backdrop') as HTMLElement;
+      if (modalBackdrop) {
+        modalBackdrop.remove();
+      }
+    }
+  }
 
 }
